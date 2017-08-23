68805be59c66587edf0cdb99cacaa5f0cab6c67c
renxiaobin <932024239@qq.com>
2017-08-23 23:44:47 +0800

    TankWar3.0 with NetWork

---
+++ TankWar3.0/src/com/rxb/tank/Direction.java
@@ -0,0 +1,5 @@
+package com.rxb.tank;
+
+public enum Direction {
+	L, LU, U, RU, R, RD, D, LD, STOP
+}
---
+++ TankWar3.0/src/com/rxb/tank/MissileNewMsg.java
@@ -0,0 +1,70 @@
+package com.rxb.tank;
+
+import java.io.ByteArrayOutputStream;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetSocketAddress;
+
+public class MissileNewMsg implements Msg{
+	int msgType = Msg.MISSILE_NEW_MSG;
+	TankClient tc;
+	Missile m;
+	
+	public MissileNewMsg(Missile m) {
+		this.m = m;
+	}
+	
+	public MissileNewMsg(TankClient tc){
+		this.tc = tc;
+	}
+
+	@Override
+	public void send(DatagramSocket ds, String IP, int udpPort) {
+		ByteArrayOutputStream baos = new ByteArrayOutputStream();
+		DataOutputStream dos = new DataOutputStream(baos);
+		try {
+			dos.writeInt(msgType);
+			dos.writeInt(m.tankId);
+			dos.writeInt(m.id);
+			dos.writeInt(m.x);
+			dos.writeInt(m.y);
+			dos.writeInt(m.dir.ordinal());
+			dos.writeBoolean(m.isGood());
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		byte[] buf = baos.toByteArray();
+		try {
+			DatagramPacket dp = new DatagramPacket(buf, buf.length,new InetSocketAddress(IP, udpPort));
+			ds.send(dp);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Override
+	public void parse(DataInputStream dis) {
+		try {
+			
+			int tankId = dis.readInt();
+			if(tankId == tc.myTank.id){
+				return;
+			}
+			int id = dis.readInt();
+			int x=dis.readInt();
+			int y = dis.readInt();
+			Direction dir = Direction.values()[dis.readInt()];
+			boolean good = dis.readBoolean();
+			
+			Missile m = new Missile(tankId,x, y, good, dir,tc);
+			m.id = id;
+			tc.missiles.add(m);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
